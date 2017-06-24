37749fb84a799c1a71956cf2b75bce41619fbc15
brock patterson <brockpatterson@gmail.com>
2017-06-24 08:12:07 -0700

    9_1_Assignment

--- app/scripts/directives/seekBar.js
+++ app/scripts/directives/seekBar.js
@@ -28,6 +28,10 @@
           return {width: percentString()};
         };
 
+        scope.thumbStyle = function(){
+          return {left: percentString()};
+        }
+
         scope.onClickSeekBar = function(event){ //updates see bar value
           var percent = calculatePercent(seekBar, event);
           scope.value = percent * scope.max;
