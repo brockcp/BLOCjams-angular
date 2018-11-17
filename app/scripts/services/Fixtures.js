29a21d14dde6093744e7d4852805864e50761b0a
brockcp <brockpatterson@gmail.com>
2018-11-17 15:04:24 -0800

    adding music

--- app/scripts/services/Fixtures.js
+++ app/scripts/services/Fixtures.js
@@ -11,7 +11,7 @@
       year: '2004',
       albumArtUrl: 'assets/images/album_covers/SummerInAbaddon.png',
       songs: [
-        { title: 'Non Photo-Blue', duration: 223, audioUrl: 'assets/music/Non-Photo-Blue'},
+        { title: 'Non Photo-Blue', duration: 34, audioUrl: 'assets/music/Non-Photo-Blue'},
         { title: 'Fortress', duration: 299, audioUrl: 'assets/music/Fortress'},
         { title: 'Syracuse', duration: 230, audioUrl: 'assets/music/Syracuse'},
         { title: 'Sender', duration: 298, audioUrl: 'assets/music/Sender'}
@@ -70,10 +70,10 @@
             year: '2004',
             albumArtUrl: 'assets/images/album_covers/SummerInAbaddon.png',
             songs: [
-              { title: 'Non Photo-Blue', duration: 223, audioUrl: 'assets/music/Non-Photo-Blue'},
-              { title: 'Fortress', duration: 299, audioUrl: 'assets/music/Fortress'},
-              { title: 'Syracuse', duration: 230, audioUrl: 'assets/music/Syracuse'},
-              { title: 'Sender', duration: 298, audioUrl: 'assets/music/Sender'}
+              { title: 'Non Photo-Blue', duration: 34, audioUrl: 'assets/music/Non-Photo-Blue'},
+              { title: 'Fortress', duration: 27, audioUrl: 'assets/music/Fortress'},
+              { title: 'Syracuse', duration: 41, audioUrl: 'assets/music/Syracuse'},
+              { title: 'Sender', duration: 33, audioUrl: 'assets/music/Sender'}
             ]
       }
     var StrangeTrails = {
@@ -84,10 +84,10 @@
             year: '2015',
             albumArtUrl: 'assets/images/album_covers/StrangeTrails.png',
             songs: [
-              {title: 'Fool For Love', duration:275, audioUrl: 'assets/music/FoolForLove'},
-              {title: 'Dead Man\'s Hand', duration:261, audioUrl: 'assets/music/DeadMansHand'},
-              {title: 'The Night We Met', duration: 203, audioUrl: 'assets/music/TheNightWeMet'},
-              {title: 'Way Out There', duration:250, audioUrl: 'assets/music/WayOutThere'}
+              {title: 'Fool For Love', duration:37, audioUrl: 'assets/music/FoolForLove'},
+              {title: 'Dead Man\'s Hand', duration:30, audioUrl: 'assets/music/DeadMansHand'},
+              {title: 'The Night We Met', duration: 22, audioUrl: 'assets/music/TheNightWeMet'},
+              {title: 'Way Out There', duration:27, audioUrl: 'assets/music/WayOutThere'}
             ]
       }
     var OkComputer = {
@@ -98,12 +98,12 @@
             year: '1997',
             albumArtUrl: 'assets/images/album_covers/OkComputer.png',
             songs: [
-              {title: 'Airbag', duration:284, audioUrl: 'assets/music/Airbag'},
-              {title: 'Paranoid Android', duration:384, audioUrl: 'assets/music/ParanoidAndroid'},
-              {title: 'Subterranean Homesick Alien', duration:268, audioUrl: 'assets/music/SubterraneanHomesickAlien'},
-              {title: 'Let Down', duration:299, audioUrl: 'assets/music/LetDown'},
-              {title: 'Karma Police', duration:262, audioUrl: 'assets/music/KarmaPolice'},
-              {title: 'No Surprises', duration:229, audioUrl: 'assets/music/NoSurprises'}
+              {title: 'Airbag', duration:38, audioUrl: 'assets/music/Airbag'},
+              {title: 'Paranoid Android', duration:50, audioUrl: 'assets/music/ParanoidAndroid'},
+              {title: 'Subterranean Homesick Alien', duration:27, audioUrl: 'assets/music/SubterraneanHomesickAlien'},
+              {title: 'Let Down', duration:32, audioUrl: 'assets/music/LetDown'},
+              {title: 'Karma Police', duration:29, audioUrl: 'assets/music/KarmaPolice'},
+              {title: 'No Surprises', duration:20, audioUrl: 'assets/music/NoSurprises'}
             ]
       };
     var SummerWinter = {
@@ -114,9 +114,9 @@
           year: '2013',
           albumArtUrl: 'assets/images/album_covers/SummerWinter.png',
           songs: [
-            {title: 'Photographs', duration:235, audioUrl: 'assets/music/Photographs'},
-            {title: 'Baby, I', duration:227, audioUrl: 'assets/music/BabyI'},
-            {title: 'Cars', duration:219, audioUrl: 'assets/music/Cars'}
+            {title: 'Photographs', duration:31, audioUrl: 'assets/music/Photographs'},
+            {title: 'Baby, I', duration:27, audioUrl: 'assets/music/BabyI'},
+            {title: 'Cars', duration:30, audioUrl: 'assets/music/Cars'}
           ]
     };
     var catalog = [SummerInAbaddon, StrangeTrails, OkComputer, SummerWinter]
